0f27d1dfa3e28b83f9e5f77202dd102ed5a8b855
songgyubin <thd0427__@naver.com>
2024-09-08 17:28:08 +0900

    [Refact] 공통 Result 모델 경량화

--- core/model/src/main/java/com/gyub/core/model/Result.kt
+++ core/model/src/main/java/com/gyub/core/model/Result.kt
@@ -1,19 +1,22 @@
 package com.gyub.core.model
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
 /**
  * 공통 Result 모델
  *
  * @author   Gyub
  * @created  2024/09/06
  */
-sealed interface Result<out D, out E> {
-    data class Success<out D>(val data: D) : Result<D, Nothing>
-    data class Error<out E>(val error: E) : Result<Nothing, E>
+sealed interface Result<out T> {
+    data class Success<T>(val data: T) : Result<T>
+    data class Error(val exception: Throwable) : Result<Nothing>
+    data object Loading : Result<Nothing>
 }
 
-inline fun <reified D, reified R, reified E> Result<D, E>.convert(callback: (data: D) -> R): Result<R, E> {
-    return when (this) {
-        is Result.Success -> Result.Success(callback(data))
-        is Result.Error -> Result.Error(this.error)
-    }
-}
+fun <T> Flow<T>.asResult(): Flow<Result<T>> = map<T, Result<T>> { Result.Success(it) }
+    .onStart { emit(Result.Loading) }
+    .catch { emit(Result.Error(it)) }
